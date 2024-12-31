07521aa8e5ad4a7bfe373ed846b22c8271004400
Apolo Garcia <apolo270garcia@gmail.com>
2024-12-31 15:17:25 -0700

    Delete legacy-ts-server

--- legacy-ts-server/src/game/constants.ts
+++
@@ -1,8 +0,0 @@
-export const GAME_WIDTH = 800;
-export const GAME_HEIGHT = 500;
-
-export const PLAYER_WIDTH = 20;
-export const PLAYER_HEIGHT = 20;
-
-export const TICK_DELAY_MS = 20;
-export const SERVER_TIME_LIMIT = 3600;  // 1 hour
