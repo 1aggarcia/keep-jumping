8a06823266395de2d0131d2110bb35170dccdf46
Apolo Garcia <apolo270garcia@gmail.com>
2025-01-01 19:51:06 -0700

    Add auto-generated socket message definitions

--- messages/generate.sh
+++ messages/generate.sh
@@ -6,13 +6,15 @@
 TS_PLUGIN_PATH="../client/node_modules/.bin/protoc-gen-ts"
 TS_OUT_PATH="../client/src/generated"
 JAVA_OUT_PATH="../server/src/main/java"
-PROTO_FILE_PATH="./pingPong.proto"
+PROTO_FILE_PATH="./socketMessage.proto"
 
-# The JS files arent needed, but protoc needs to generate them for the TS file
 protoc \
     --plugin="protoc-gen-ts=$TS_PLUGIN_PATH" \
     --ts_opt=esModuleInterop=true \
     --java_out=$JAVA_OUT_PATH \
-    --js_out=$TS_OUT_PATH/js \
+    --js_out=$TS_OUT_PATH \
     --ts_out=$TS_OUT_PATH \
     $PROTO_FILE_PATH
+
+# The JS files arent needed, but protoc needs to generate them for the TS file
+rm ../client/src/generated/*.js
