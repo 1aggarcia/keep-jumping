0169ba0f5ae9dbd1eef28f97298fee1fc1135b34
Apolo Garcia <apolo270garcia@gmail.com>
2025-04-13 19:55:18 -0600

    Improve leaderboard date display and pre-fetch state

--- client/src/ui/dom.ts
+++ client/src/ui/dom.ts
@@ -1,7 +1,7 @@
 import $ from "jquery";
 import { GAME_HEIGHT, GAME_WIDTH } from "./gameConstants";
 import { AppState, LeaderboardEntry } from "../types";
-import { formatBytesString } from "./formatters";
+import { formatBytesString, getRelativeAgeString } from "./formatters";
 
 const GAME_ASPECT_RATIO = GAME_WIDTH / GAME_HEIGHT;
 const LEADERBOARD_ROWS = 10;
@@ -91,7 +91,7 @@ export function fillLeaderboard(entries: LeaderboardEntry[]) {
             playerCell.textContent = nextEntry.player;
             scoreCell.textContent = String(nextEntry.score);
             timestampCell.textContent =
-                new Date(nextEntry.timestamp).toLocaleDateString();
+                getRelativeAgeString(new Date(nextEntry.timestamp));
         } else {
             playerCell.textContent = "";
             scoreCell.textContent = "";
--- client/src/ui/formatters.ts
+++ client/src/ui/formatters.ts
@@ -42,3 +42,53 @@ export function getPrettyMessage(message: unknown) {
         return message;
     }
 }
+
+const MS_PER_SECOND = 1000;
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
+const SECONDS_PER_DAY = SECONDS_PER_HOUR * 24;
+const SECONDS_PER_WEEK = SECONDS_PER_DAY * 7;
+const SECONDS_PER_YEAR = SECONDS_PER_WEEK * 52;
+
+/**
+ * Returns a string such as "5 months ago" or "3 minutes ago" depending on
+ * how much time has passed since the passed in date
+ * @param timestamp 
+ */
+export function getRelativeAgeString(timestamp: Date) {
+    const timestampSeconds = timestamp.getTime() / MS_PER_SECOND;
+    const nowSeconds = Date.now() / MS_PER_SECOND;
+    if (timestampSeconds > nowSeconds) {
+        console.error(` Tried to get age of future timestamp: ${timestamp}`);
+        return timestamp.toLocaleDateString();
+    }
+
+    const secondsPassed = nowSeconds - timestampSeconds;
+
+    let divisor: number;
+    let unit: string;
+    if (secondsPassed < SECONDS_PER_MINUTE) {
+        divisor = 1;
+        unit = "second"
+    } else if (secondsPassed < SECONDS_PER_HOUR) {
+        divisor = SECONDS_PER_MINUTE;
+        unit = "minute";
+    } else if (secondsPassed < SECONDS_PER_DAY) {
+        divisor = SECONDS_PER_HOUR;
+        unit = "hour";
+    } else if (secondsPassed < SECONDS_PER_WEEK) {
+        divisor = SECONDS_PER_DAY;
+        unit = "day";
+    } else if (secondsPassed < SECONDS_PER_YEAR) {
+        divisor = SECONDS_PER_WEEK;
+        unit = "week";
+    } else {
+        divisor = SECONDS_PER_YEAR;
+        unit = "year";
+    }
+
+    const timeInterval = Math.floor(secondsPassed / divisor);
+    const pluralIndicator = timeInterval === 1 ? "" : "s";
+
+    return `${timeInterval} ${unit}${pluralIndicator} ago`;
+}
